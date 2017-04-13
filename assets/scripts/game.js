fe8975f66667bac669a19fd61ab7f8319359a7b9
pjliddy <pjliddy@gmail.com>
2017-04-13 15:41:16 -0400

    Clean up and optimize game engine code
    Added comments and removed debug statements

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,15 +1,19 @@
 'use strict'
 const api = require('./auth/api')
 const ui = require('./auth/ui')
-// const store = require('./store.js')
-
 const views = require('./views.js')
 
+// map unicode chars with better shapes for X and O
 const xChar = '\u2715'
 const oChar = '\u3007'
+
+// define game variables
 const moves = new Array(9)
 let turn = 0
 let player = 'x'
+let over = false
+
+// define "cues" to check for win state
 const cues = [
   [0, 1, 2],
   [3, 4, 5],
@@ -21,24 +25,35 @@ const cues = [
   [2, 4, 6]
 ]
 
-const initGame = function () {
-  console.log('initGame')
+// initGame()
+// initialize game engine
 
+const initGame = function () {
   moves.fill('')
   turn = 0
   player = 'x'
   views.message('player ' + player + '\'s turn')
 }
 
-const chooseSquare = function (evt) {
-  const sqNum = $(this).data('id')
-  moves[sqNum] = player.toLowerCase()
-  updateSquare(this)
-  console.log('turn ', turn)
-  const status = gameStatus()
-  console.log('chooseSquare(): status = ', status)
-  let over = false
+// chooseSquare()
+// handle user's choice of square in the game
 
+const chooseSquare = function (evt) {
+  // get which square the player clicked on
+  const sqNum = $(this).data('id')
+  // store the move in the moves array
+  moves[sqNum] = player.toLowerCase()
+  // update game and UI
+  updateSquare(this)
+  // determine if game is over
+  const status = gameStatus()
+  // set over flag for update API call
+  if (!status) {
+    over = status
+  } else {
+    over = true
+  }
+  // set values of data object for update API call
   const data = {
     'game': {
       'cell': {
@@ -49,29 +64,27 @@ const chooseSquare = function (evt) {
     }
   }
 
-  // handle turn
+  // TO DO: IMPROVE FLOW OF THIS CODE:
+  //  1. handle turn
+  //  2. evaluate game
+  //  3. update indexes
+  //  4. dispay result
 
-  // evaluate game
-
-  // update indexes
-
-  // dispay result
-
+  // if game is not over yet
   if (status === 0 && turn < 8) {
-    // game is not over yet
-    turn++
-    togglePlayer()
-    data.game.over = over
-
+    // save move with API
     api.updateGame(data)
       .then(ui.updateGameSuccess)
       .catch(ui.updateGameFailure)
+
+    // update turn and player
+    turn++
+    togglePlayer()
   } else {
     // game over, man!
-    views.onGameOverView()
     over = true
-
     data.game.over = over
+    views.onGameOverView()
 
     api.updateGame(data)
       .then(ui.updateGameSuccess)
@@ -82,39 +95,41 @@ const chooseSquare = function (evt) {
 
     if (status !== 0) {
       views.message('player ' + player.toUpperCase() + ' won')
+      // if player 'x' won
       if (player === 'x') {
+        // get player's game tally from the API
         api.getIndex(data)
           .then(ui.getIndexSuccess)
           .catch(ui.getIndexFailure)
       }
     } else {
+      // update message to player in UI
       views.message('tie game')
     }
   }
-
-  // data.over = over
-  //
-  // api.updateGame(data)
-  //   .then(ui.updateGameSuccess)
-  //   .catch(ui.updateGameFailure)
-
-  // DOES THIS NEED TO BE NULLED OR NOT?
-  // store.game = null
 }
 
+// updateSquare()
+// update UI based on user's choice of square
+
 const updateSquare = function (sqObj) {
-  // set char
+  // set char on the board to the right symbol for current player
   let playerChar
+
   if (player === 'x') {
     playerChar = xChar
   } else {
     playerChar = oChar
   }
 
+  // apply jQuery cross fade to content change
   $(sqObj).fadeOut(100, function () {
     $(sqObj).closest('.square').html(playerChar).fadeIn(100)
   })
 }
+
+// togglePlayer()
+// swith between 'x' and 'o' player for next turn
 
 const togglePlayer = function () {
   if (player === 'x') {
@@ -122,17 +137,23 @@ const togglePlayer = function () {
   } else {
     player = 'x'
   }
+  // update message to player in UI
   views.message('player ' + player.toUpperCase() + '\'s turn')
 }
 
+// checkCue()
+// checks a "cue" of three squares to see if they indicate a winning condition
+
 const checkCue = function (cue) {
+  // see if all three squares are the same
   if (
     moves[cue[0]] === moves[cue[1]] &&
     moves[cue[0]] === moves[cue[2]] &&
     moves[cue[1]] === moves[cue[2]]
   ) {
-    // all three are the same (return 1 for 'X', -1 for 'O', or 0 for tie)
+    // if the content of the squares in the cue is not blank
     if (moves[cue[0]]) {
+      // all three are the same (return 'x', 'o', or 0 for tie)
       if (moves[cue[0]] === 'x') {
         return 'x'
       } else {
@@ -144,26 +165,28 @@ const checkCue = function (cue) {
   }
 }
 
+// gameStatus()
+// checks all cues and returns "x" or "o" if there's a winner or 0 for a tie
+
 const gameStatus = function () {
   for (let i = 0; i < cues.length; i++) {
     const winner = checkCue(cues[i])
     if (winner) {
-      // console.log('gameStatus(): winner = ', winner)
       return winner
     }
   }
   return 0
 }
 
+// disableEmptySquares()
+// when the game is over, remove any remaing buttons from board squares
+
 const disableEmptySquares = function () {
   // find empty values in turns[]
-
   for (let i = 0; i < moves.length; i++) {
     if (moves[i] !== '') {
-      // get square divs with data-id = i
-      // $('[data-id="' + i + '"]').html('')
-      // $('.play-btn[data-id]').closest('.square').html('')
-
+      // clear the content of squares that still have buttons
+      // apply jQuery cross fade to content change
       $('.play-btn[data-id]').fadeOut(100, function () {
         $('.play-btn[data-id]').closest('.square').html('').fadeIn(100)
       })
