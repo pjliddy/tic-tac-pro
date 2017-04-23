aa06126daf33245b80e2b011995d7198efb26e8d
pjliddy <pjliddy@gmail.com>
2017-04-23 19:31:16 -0400

    Rolled back to previous view control pattern
    Needs a global view controller accessible from game or application level

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,24 +2,18 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-// const board = require('./board')
+const authEvents = require('./auth/events.js')
+const views = require('./views.js')
 
 $(() => {
   setAPIOrigin(location, config)
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
 // use require without a reference to ensure a file is bundled
 require('./example')
-
-const authEvents = require('./auth/events.js')
-const views = require('./views.js')
 
 // On document ready
 $(() => {
   authEvents.addHandlers()
-  // initialize application view states
   views.init()
 })
